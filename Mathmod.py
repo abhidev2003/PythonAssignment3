386a7e822395179919fca40713f6c961c28a3224
Abhidev S P <abhidevsp2003@gmail.com>
2025-05-30 22:55:46 +0530

    Add files via upload

---
+++ Mathmod.py
@@ -0,0 +1,6 @@
+import math
+
+n = int(input("Enter a number: "))
+print(f"The square root of {n} is {math.sqrt(n)}")
+print(f"Natural log of {n} to base of e is {math.log(n,math.e)}")
+print(f"Sine of [n] in radian is {math.sin(n)}")
